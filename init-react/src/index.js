754a08e77538649b812da7e0392353b7d879eda6
Hannibal <wen-shun.han@dxc.com>
2022-03-31 17:00:34 +0800

    basically react js project

--- init-react/src/index.js
+++
@@ -1,9 +0,0 @@
-import position from './position/index.js'
-
-const profile = {
-  name: '神Ｑ超人',
-  position: 'Front End Development',
-};
-
-console.log(`Hi! My name is ${profile.name}`);
-console.log(`position is ${position}`);
--- init-react/webpack.config.js
+++ init-react/webpack.config.js
@@ -1,8 +1,65 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './dist/'),
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /.jsx$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: { localIdentName: '[name]__[local]___[hash:base64:5]' },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './index.css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/index.html", to: "index.html" ,toType: "file"},
+      ],
+    }),
+  ],
+  devServer: {
+    // contentBase: './dist',
+    static:'./dist',
+    port: 8080,
+  },
 };
